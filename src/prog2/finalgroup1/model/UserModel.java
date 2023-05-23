197f6b0a83b64e5c7301758c1ef964d3ddc9cd3b
Zhecto <124892220+Zhecto@users.noreply.github.com>
2023-05-23 18:49:32 +0800

    Update UserModel.java

--- src/prog2/finalgroup1/model/UserModel.java
+++ src/prog2/finalgroup1/model/UserModel.java
@@ -1,11 +1,62 @@
 package prog2.finalgroup1.model;
+package prog2.finalgroup1.model;
 
 public class UserModel {
     private String username;
     private String password;
 
+    /**
+     *
+     * @param username
+     * @param password
+     */
     public UserModel(String username, String password) {
         this.username = username;
+        this.password = password;
+    }
+
+    /**
+     *
+     * @param user
+     */
+    public UserModel(UserModel user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     *
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     *
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
         this.password = password;
     }
 
