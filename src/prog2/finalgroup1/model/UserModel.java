c824f9878e5633ebf9a92b10040f347ac58ca279
Zhecto <124892220+Zhecto@users.noreply.github.com>
2023-05-23 18:48:28 +0800

    Update UserModel.java
    commetms

--- src/prog2/finalgroup1/model/UserModel.java
+++ src/prog2/finalgroup1/model/UserModel.java
@@ -4,28 +4,53 @@ public class UserModel {
     private String username;
     private String password;
 
+    /**
+     *
+     * @param username
+     * @param password
+     */
     public UserModel(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    /**
+     *
+     * @param user
+     */
     public UserModel(UserModel user) {
         this.username = user.getUsername();
         this.password = user.getPassword();
     }
 
+    /**
+     *
+     * @return
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     *
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
